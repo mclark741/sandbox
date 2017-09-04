f8fbf569686a8a7c533bfd7e0178e50e244b555e
mclark741 <mclark@tmtl.com>
2017-09-03 20:29:43 -0500

    initial commit

---
+++ SampleWeb/site.js
@@ -0,0 +1,21 @@
+function changeText() {
+
+    const header = document.querySelector('h1');
+    header.innerText = "Clicked the button"
+    console.log("changed text");
+}
+
+const user = {
+    name: 'matthew',
+    username: 'mclark'
+};
+
+const users = ['matt', 'nick', 'holly'];
+let numbers = [1, 43, 54, 132];
+numbers = 3;
+
+
+const original = 'matt';
+const clone = 'MATT';
+
+console.log(user);
---
+++ chirp/javascripts/chirpApp.js
@@ -0,0 +1,14 @@
+console.log(Date.now());
+
+var app = angular.module('chirpApp', []);
+
+app.controller('mainController', function($scope){
+  $scope.posts = [];
+  $scope.newPost = {created_by: '', text: '', created_at: ''};
+  
+  $scope.post = function(){
+    $scope.newPost.created_at = Date.now();
+    $scope.posts.push($scope.newPost);
+    $scope.newPost = {created_by: '', text: '', created_at: ''};
+  };
+});
