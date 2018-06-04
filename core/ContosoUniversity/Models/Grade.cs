95e7375ce52a80f5937c2def02cc6e33c2ff0d06
Matt Clark <matt.clark@buildasign.com>
2018-06-04 15:46:00 -0500

    check in step 5 of 10 - https://docs.microsoft.com/en-us/aspnet/core/data/ef-mvc/read-related-data?view=aspnetcore-2.0

---
+++ core/ContosoUniversity/Models/Grade.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Models
+{
+    public enum Grade
+    {
+        A,
+        B,
+        C,
+        D,
+        F
+    }
+}
